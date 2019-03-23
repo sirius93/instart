e23a211e59c4f9fe56f5c96fdf580501bf1e9afb
Nandan Kumar <nkumar101@sapient.com>
2019-03-23 13:38:46 +0530

    minor changes

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -7,16 +7,37 @@ class Blog extends Component {
       super(props)
   }
 
+getRandomDate = () => {
+    let date = new Date(+(new Date()) - Math.floor(Math.random()*10000000000)),
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        year = date.getFullYear(),
+        month = months[date.getMonth()],
+        day = date.getDate();
+
+    return `${month} ${day}, ${year}`;
+}
+getRandomTag = () => {
+    let tags = ['lifestyle','travelling', 'nightlife'],
+        tag = tags[Math.floor(Math.random()*tags.length)];
+        
+    return tag;    
+}
+
 render() {
-let imgSrc = this.props.image ? this.props.image : '';
-let TemplatewithImg = imgSrc ?  <div>
+let imgSrc = this.props.image ? this.props.image : '',
+    randomDate = this.getRandomDate(),
+    randomTag = this.getRandomTag();
+
+const TemplatewithImg = imgSrc ?  <div>
                                     <div className="image-container"> 
                                         <img className="blog-image" src={imgSrc}></img>  
                                     </div>
                                     <i class="fa fa-image icon-image"></i>
+                                    <div className="tag">{randomTag}</div>
                                     <div className="title-container">
                                         <h6 className="blog-title">{this.props.title}</h6>
-                                    </div> 
+                                    </div>
+                                    <div className="date">{randomDate}</div>
                                     <div className="link-container">
                                         <a href="" className="read-more-btn">Read more</a>
                                     </div>
@@ -25,10 +46,11 @@ let TemplatewithImg = imgSrc ?  <div>
                                 : 
                                 <div>
                                     <i class="fa fa-pencil edit-icon"></i>
+                                    <div className="tag">{randomTag}</div>
                                     <div className="title-container">
                                         <h6 className="blog-title">{this.props.title}</h6>
                                     </div> 
-                                    
+                                    <div className="date">{randomDate}</div>
                                     <div className="blog-body-container">
                                         <p className="blog-body">{this.props.body}</p>
                                     </div>
